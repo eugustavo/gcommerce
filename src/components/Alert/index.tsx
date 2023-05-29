a03edf575559ee673608dbf329ee3fc1db6c66fe
eugustavo <gustavo.ifto@gmail.com>
2023-05-29 15:26:21 -0300

    feat: create alert for when remove product

---
+++ src/components/Alert/index.tsx
@@ -0,0 +1,60 @@
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  Text,
+} from '@chakra-ui/react'
+
+interface AlertProps {
+  isOpen: boolean
+  onClose: () => void
+  onConfirm: () => void
+  cancelRef: any
+
+  title: string
+  description: string
+}
+
+export function Alert({
+  title,
+  description,
+  isOpen,
+  onClose,
+  onConfirm,
+  cancelRef,
+}: AlertProps) {
+  return (
+    <AlertDialog
+      isOpen={isOpen}
+      leastDestructiveRef={cancelRef}
+      onClose={onClose}
+    >
+      <AlertDialogOverlay>
+        <AlertDialogContent>
+          <AlertDialogHeader fontSize="lg" fontWeight="bold">
+            {title}
+          </AlertDialogHeader>
+
+          <AlertDialogBody>
+            <Text fontSize="16px" color="textLow">
+              {description}
+            </Text>
+          </AlertDialogBody>
+
+          <AlertDialogFooter>
+            <Button ref={cancelRef} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="red" onClick={onConfirm} ml={3}>
+              Delete
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialogOverlay>
+    </AlertDialog>
+  )
+}
